de6bb4777bbcbfa921492193f8d15b58d18bde55
rinbo <robin.b@outlook.com>
2018-09-20 22:41:27 +0200

    Add new and create actions in articles controller and define a form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,23 @@
 class ArticlesController < ApplicationController
+ 
   def index
     @articles = Article.all
   end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to root_path 
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
+  
 end
 
