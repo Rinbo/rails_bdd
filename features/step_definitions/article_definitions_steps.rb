1553fe9e8221998ad097c3bd5fd7d33cc08d129a
rinbo <robin.b@outlook.com>
2018-09-20 22:46:28 +0200

    Add definitions for creating articles and rename step definitionsn file

---
+++ features/step_definitions/article_definitions_steps.rb
@@ -0,0 +1,30 @@
+Given("the following articles exists") do |table|
+  table.hashes.each do |article|
+    Article.create!(article)
+  end
+end
+
+When("I visit the landing page") do
+  visit root_path
+end
+
+Then("I should see {string}") do |content|
+  expect(page).to have_content content
+end
+
+When("I click {string} link") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I fill in {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click {string} button") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be on {string} page") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
