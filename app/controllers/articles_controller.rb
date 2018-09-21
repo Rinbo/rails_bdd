ae6efdbd341f1fdbb430217eea0657c14fc010ae
rinbo <robin.b@outlook.com>
2018-09-21 14:01:11 +0200

    Add edit form and controller actions to update an existing article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,15 +12,29 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  def create
-    flash[:notice] = "Article was successfully created."
+  def edit
+    @article = Article.find(params[:id])
+  end
 
+  def create
+    
     @article = Article.new(article_params)
     if @article.save
       redirect_to @article
+      flash[:notice] = "Article was successfully created."
     else
       render 'new'
     end    
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+      flash[:notice] = "Article was successfully updated."
+    else
+      render 'edit'
+    end
   end
 
   private
